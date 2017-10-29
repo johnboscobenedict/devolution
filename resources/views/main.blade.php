f43320048eff306224b331b06e4904ba710d027e
dogstyle007 <johnbosco.benedict8@gmail.com>
2017-10-29 10:04:16 +0000

    first commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Devolution</title>
+
+    <!-- Bootstrap core CSS -->
+
+    <!-- Custom styles for this template -->
+    <link href="/css/app.css" rel="stylesheet">
+
+    @include('layouts.nav')
+
+  </head>
+
+  <body>
+
+
+    @yield('content')
+
+    
+
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="/js/jquery-3.2.1.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+
+
+  </body>
+</html>
